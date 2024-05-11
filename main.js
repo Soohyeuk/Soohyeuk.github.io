2b247902c4424023c6fa16441d9a70c046c3b016
Soohyeuk <csh040816@gmail.com>
2024-05-11 17:55:49 -0400

    nav bar for mobile updated

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-
-let sections = document.querySelectorAll("section");
-
-window.onscroll = () => {
-    sections.forEach( sec => {
-        let top = window.scrollY; 
-        let offset = sec.offsetTop;
-        let height = sec.offsetHeight;
-
-        if (top >= offset && top < offset + height) {
-            sec.classList.add('show-animate');
-        } else {
-            sec.classList.remove('show-animate');
-        }
-    })
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".mobile")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
 }
